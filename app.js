fbca00c54fe416fa89be0a5011d79c29e7d56685
hac <hactheplanet@gmail.com>
2015-12-27 00:19:19 +0100

    Forgot to test! It's working now ;)

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ io.on('connection', function(socket){
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+http.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
