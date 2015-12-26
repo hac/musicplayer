1c3b0d4c107cdea31b2051bc6960f26948343f30
hac <hactheplanet@gmail.com>
2015-12-27 00:09:58 +0100

    Prepare for OpenShift.

--- app.js
+++ app.js
@@ -45,5 +45,9 @@ io.on('connection', function(socket){
   });
 });
 
-
-http.listen(3000)
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
